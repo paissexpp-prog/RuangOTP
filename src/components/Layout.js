88160b8ddd2c8742d25b481ed785aadb5d64f199
paissexpp-prog <paissexpp@gmail.com>
2025-12-31 22:07:58 +0700

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,18 @@
-import Navbar from './Navbar'; // Navbar atas
-import BottomNav from './BottomNav'; // <--- Import ini
+// Pastikan baris import ini ada!
+import Navbar from './Navbar';
+import BottomNav from './BottomNav'; // <--- JANGAN LUPA INI
 
 export default function Layout({ children }) {
   return (
-    <div className="bg-gray-50 min-h-screen pb-20"> {/* Tambah padding bawah (pb-20) biar konten gak ketutup menu */}
+    <div className="bg-gray-50 min-h-screen pb-24"> {/* Tambah padding bawah biar konten ga ketutup */}
       <Navbar />
+      
       <main className="container mx-auto p-4">
         {children}
       </main>
-      <BottomNav /> {/* <--- Pasang disini */}
+
+      {/* PASTIIN KOMPONEN INI DIPASANG DISINI */}
+      <BottomNav /> 
     </div>
   );
 }
